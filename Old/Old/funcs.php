e8ac935c2b0e2d9789a66d882d120ba2161f6e2c
Yoji Mochizuki <Mochizuki.Yoji@dynamic-maps.co.jp>
2025-02-18 22:48:54 +0900

    不要ファイル削除

--- Old/Old/funcs.php
+++ Old/Old/funcs.php
@@ -3,9 +3,8 @@
 //XSS対応（ echoする場所で使用！それ以外はNG ）
 function h($str)
 {
-   return htmlspecialchars($str ?? '', ENT_QUOTES, 'UTF-8');
+    return htmlspecialchars($str, ENT_QUOTES);
 }
-
 
 //DB接続
 function db_conn()
