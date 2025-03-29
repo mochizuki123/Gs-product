e0c6310d506b2772b2c393b459e6dc36913ac83a
mochizuki123 <mochizuki.yoji@dynamic-maps.co.jp>
2025-03-29 19:39:58 +0900

    Delete 250329更新(1) directory

--- 250329更新(1)/reset_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['speech_data']);
-// もしくはセッション全体をクリアする場合は session_destroy();
-echo "セッションがリセットされました。";
-?>
--- 250329更新(1)/reset_session1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['experience']);
-unset($_SESSION['interest']);
-unset($_SESSION['theme']);
-unset($_SESSION['text_theme']);
-echo json_encode(['status' => 'success', 'message' => 'セッションデータが削除されました。']);
-exit();
-?>
